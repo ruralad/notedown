e429e3a1f1945bef1fd83ad32f2c162cbbec9068
ruralad <avabhinav13@gmail.com>
2023-04-21 23:24:32 +0530

    New Loading screen

--- src/components/UtilitySection/Loading.tsx
+++ src/components/UtilitySection/Loading.tsx
@@ -1,12 +1,12 @@
 const Loading = () => {
   return (
     <div className="absolute grid place-items-center bg-zinc-800 w-screen text-white h-screen z-999999">
-      <dotlottie-player
-        src="./src/assets/book.lottie"
-        autoplay
-        loop
-        style={{ height: "50%", width: "50%" }}
-      />
+      <div className="grid place-items-center">
+        <div className="loading"></div>
+        <span className="mt-2 text-gray-400 text-sm">
+          loading your workspace
+        </span>
+      </div>
     </div>
   );
 };
