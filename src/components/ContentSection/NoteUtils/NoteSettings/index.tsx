9ae947e30649653ed577f04fd2c53735bde72ccc
ruralad <avabhinav13@gmail.com>
2023-04-29 11:07:09 +0530

    feat: Copy to Clipboard
    - folder refactor
    - removed pnpm workspace
    - readme update to include license details

--- src/components/ContentSection/NoteUtils/NoteSettings/index.tsx
+++ src/components/ContentSection/NoteUtils/NoteSettings/index.tsx
@@ -1,24 +1,36 @@
 import * as Dialog from "@radix-ui/react-dialog";
 
-import { useActiveStore } from "../../../store/NoteStore";
-import { useSettingsStore } from "../../../store/SettingsStore";
+import { useActiveStore } from "../../../../store/NoteStore";
+import { useSettingsStore } from "../../../../store/SettingsStore";
 
 import {
   updateAppSettings,
   updateDeletedNotesCount,
-} from "../../../utils/StatsUtils";
-import { deleteNote } from "../../../utils/WriteUtils";
+} from "../../../../utils/StatsUtils";
+import { deleteNote } from "../../../../utils/WriteUtils";
 
+import { useEffect, useState } from "react";
 import { AiOutlineDelete } from "react-icons/ai";
 import { BsMarkdown } from "react-icons/bs";
 import { FiEdit3 } from "react-icons/fi";
-import { AppSettingsProps } from "../../../../types/Settings";
+import { TbClipboardCopy } from "react-icons/tb";
+import { AppSettingsProps } from "../../../../../types/Settings";
+import { copyToClipboard } from "../../../../utils/ReadUtils";
 
 const NoteSettings = () => {
   const activeNoteTitle = useActiveStore((state) => state.activeNoteTitle);
   const appSettings = useSettingsStore((state) => state.appSettings);
 
   const setAppSettings = useSettingsStore((state) => state.setAppSettings);
+
+  const [copiedToClipboard, setCopiedToClipboard] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (copiedToClipboard) setCopiedToClipboard(false);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [copiedToClipboard]);
 
   const handleEditorStyle = (style: "code" | "markdown") => {
     const newSettings: AppSettingsProps = {
@@ -29,20 +41,49 @@ const NoteSettings = () => {
     setAppSettings(newSettings);
   };
 
+  const handleCopyToClipboard = async () => {
+    setCopiedToClipboard(true);
+    copyToClipboard(activeNoteTitle);
+  };
+
   if (activeNoteTitle.length > 0) {
     return (
       <div className="absolute z-999 flex gap-3 right-3 top-3 text-gray-400">
-        {!(appSettings.editorStyle === "markdown") ? (
+        {!!(appSettings.editorStyle === "markdown") ? (
+          <FiEdit3
+            className="hover:cursor-pointer"
+            title="Switch to Editor"
+            onClick={() => handleEditorStyle("code")}
+          />
+        ) : (
           <BsMarkdown
             className="hover:cursor-pointer"
             title="View as Markdown"
             onClick={() => handleEditorStyle("markdown")}
           />
+        )}
+        {!!copiedToClipboard ? (
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 120.2 120.2"
+            className="w-4"
+          >
+            <polyline
+              className="checkmark"
+              fill="none"
+              stroke="#ffffff"
+              stroke-width="6"
+              stroke-linecap="round"
+              stroke-miterlimit="10"
+              points="100.2,40.2 51.5,88.8 29.8,67.5 "
+            />
+          </svg>
         ) : (
-          <FiEdit3
+          <TbClipboardCopy
             className="hover:cursor-pointer"
-            title="Switch to Editor"
-            onClick={() => handleEditorStyle("code")}
+            title="Copy to Clipboard"
+            onClick={handleCopyToClipboard}
           />
         )}
         {/* <BiExport
--- src/utils/ReadUtils.ts
+++ src/utils/ReadUtils.ts
@@ -1,3 +1,4 @@
+import { writeText } from "@tauri-apps/api/clipboard";
 import { readDir, readTextFile } from "@tauri-apps/api/fs";
 
 import { getNotedownFolder, verifyNotedownFolder } from "./DirectoryUtils";
@@ -17,4 +18,9 @@ export const readNote = async (noteName: string) => {
   const contents = await readTextFile(folder + `\\` + noteName);
   const json: NoteProps = JSON.parse(contents);
   return json;
+};
+
+export const copyToClipboard = async (noteName: string) => {
+  const json = await readNote(noteName);
+  await writeText(json.title + "\n" + json.content);
 };
